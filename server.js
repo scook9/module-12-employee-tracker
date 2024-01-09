5911b5f30a6c8e123e777d3c967a80200b847cd2
[Spencer Cook] <spencerrcook@hotmail.com>
2024-01-08 17:31:14 -0700

    added MySQL tables

--- server.js
+++ server.js
@@ -2,3 +2,28 @@
 //inquirer from "npm i inquirer@8.2.4"
 //express
 //MySQL2, asynchronous queries
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username",
+    },
+    {
+      type: "password",
+      message: "What is your password?",
+      name: "password",
+    },
+    {
+      type: "password",
+      message: "Re-enter password to confirm:",
+      name: "confirm",
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log("Success!")
+      : console.log("You forgot your password already?!")
+  );
